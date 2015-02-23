4946e42e0fa673c9c8da477632df9110b8fd631e
Durant <jamesdrenda@charter.net>
2015-02-23 01:18:46 +0000

    Working panel.arrows example. Would like to make custom panel function. Started adding additional tables of types of plots.

--- testXYPLOT.R
+++ testXYPLOT.R
@@ -1,15 +1,38 @@
 require(latticeExtra)
 require(openair)
 library(circular)
-testData <- selectByDate(mydata, day = 1, month=1, year=2000)
-testData$u <- sin(rad(testData$wd))  * testData$ws
-testData$v <- cos(rad(testData$wd)) * testData$ws
 
-plt <- xyplot(nox~date, data=testData, type="g", aspect="xy")
-plt <- plt + layer(panel.arrows(x0=testData$date,y0=testData$nox, x1 = testData$date + testData$u,
-                                y1= testData$nox + testData$v, length=0.1))
-plt + layer(panel.text(x=testData$date,y=testData$nox, labels=testData$wd))
+Sys.setenv(TZ='GMT')
+
+testData <- selectByDate(mydata, start="1/7/1999", end="2/7/1999")
+
+plt <- xyplot(pm10~date, data=testData, type="g", xlab="Date", ylab=quickText("pm10 (ug.m-3)"))
+
+deltaX <- as.numeric(plt$x.limits[[2]]) - as.numeric(plt$x.limits[[1]])
+deltaY <- as.numeric(plt$y.limits[[2]]) - as.numeric(plt$y.limits[[1]])
+
+pltRatio <- plt$aspect.ratio
+ratio <- deltaY/deltaX
+factor = 3600
+length = 0.05
+
+#scale wind speed
+scaleWS <- testData$ws/max(testData$ws, na.rm=TRUE)
+
+testData$u <- sin(rad(testData$wd))  * factor * scaleWS
+testData$v <- cos(rad(testData$wd)) *  factor * ratio/pltRatio * scaleWS
+
+plt2 <- xyplot(pm10~date, data=testData, x0=testData$date + testData$u ,y0=testData$pm10 + testData$v, x1 = testData$date - testData$u,
+               y1= testData$pm10 - testData$v, length=length, labels=testData$wd,
+               panel=function(...) {
+                 panel.arrows(...)
+                 panel.grid(...)
+                 #panel.text(...)
+                 },
+               xlab="Date", ylab=quickText("PM10 (ug.m-3)"),
+               main=quickText("Winds by Date and PM10")
+               )
+plt2
 
 
-plt2 <- xyplot(nox ~ date, data=testData, type="g", labels=testData$wd)
-update(plt2, panel=function(...) panel.text(...))
+
